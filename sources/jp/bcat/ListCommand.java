61483d19aa237899dbf6ee9a02e2478f342eb7c4
山村歩 <ayugooayu@gmail.com>
2020-01-06 21:49:53 +0900

    更新

--- sources/jp/bcat/ListCommand.java
+++ sources/jp/bcat/ListCommand.java
@@ -17,8 +17,29 @@ public class ListCommand implements CLICommand{
 	}
 
 	public void process(BufferedReader in,PrintWriter out)throws IOException{
+		Book result[] = catalog.getBooks();
+		out.println("合計" + result.length + "項目");
+		BookWriter w = new BookWriter(out," ");
+		for(int i = 0; i < result.length; i++) {
+			out.println("[" + (i + 1) + "]:");
+			out.println("図書ID："+ result[i].getBookId());
+			w.write(result[i]);
+			out.println();
+		}
+	}
 
-
+	public static void main(String args[]) {
+		try {
+			BufferedReader in = new BufferedReader(
+					new InputStreamReader(System.in));
+			PrintWriter out = new PrintWriter(
+					new OutputStreamWriter(System.out), true);
+			BookCatalog catalog = new BookCatalog();
+			ListCommand command = new ListCommand(catalog);
+			command.process(in, out);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 }
 
